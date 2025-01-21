5bcd820ad3b918fd793d7dd7be34fc3c1711aa79
Dipak4064 <orraasss@gmail.com>
2025-01-21 21:40:29 +0545

    file update

--- expo_practice.java
+++ expo_practice.java
@@ -1,21 +1,63 @@
 
 public class expo_practice {
 
-    public static int fibboNacci(int n) {
-        if (n == 0 || n == 1) {
-            return n;
+    public static void merge(String arr[], int l, int mid, int r) {
+        int n1 = mid - l + 1;
+        int n2 = r - mid;
+        String L[] = new String[n1];
+        String R[] = new String[n2];
+
+        for (int i = 0; i < n1; i++) {
+            L[i] = arr[l + i];
         }
-        int p = fibboNacci(n - 1);
-        int l = fibboNacci(n - 2);
-        int ans = p + l;
-        return ans;
+        for (int j = 0; j < n2; j++) {
+            R[j] = arr[mid + 1 + j];
+        }
+
+        int i = 0, j = 0;
+        int k = l;
+
+        while (i < n1 && j < n2) {
+            if (L[i].compareTo(R[j]) <= 0) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        // Copy remaining elements of L[], if any
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        // Copy remaining elements of R[], if any
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    public static void mergeSort(String arr[], int l, int r) {
+        if (l >= r) {
+            return;
+        }
+        int mid = l + (r - l) / 2;
+        mergeSort(arr, l, mid);
+        mergeSort(arr, mid + 1, r);
+        merge(arr, l, mid, r);
     }
 
     public static void main(String[] args) {
-        int x = 2, n = 6;
-        // System.out.println(OptimizedPower(n));
-        int M = fibboNacci(n);
-        // System.out.println(fibboNacci(n));
-        System.out.println(M);
+        String arr[] = {"shyam", "aayush", "dipak"};
+        mergeSort(arr, 0, arr.length - 1);
+        for (String s : arr) {
+            System.out.print(s + " ");
+        }
     }
 }
