5b6ae810d705e6fea5d0e9f31ff44c99d41448a7
Dipak4064 <orraasss@gmail.com>
2025-01-22 11:25:53 +0545

    update

---
+++ BackTracking/string_Permutations.java
@@ -0,0 +1,20 @@
+class string_Permutations{
+    public static void permute(String str, String ans){
+        //base case
+        if(str.length()==0){
+            System.out.println(ans);
+            return;
+        }
+        // recursion
+        for(int i=0;i<str.length();i++){
+            char curr = str.charAt(i);
+            String newStr = str.substring(0,i)+str.substring(i+1,str.length());
+            permute(newStr,ans+curr);
+        }
+    }
+    public static void main(String[] args) {
+        String str = "ABC";
+        int n = str.length();
+        permute(str, "");
+    }
+}
