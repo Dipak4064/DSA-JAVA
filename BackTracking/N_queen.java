e2d203e114379a1837e3da37e31d3c8b3b23dcbb
Dipak4064 <orraasss@gmail.com>
2025-01-24 21:10:45 +0545

    file update

--- BackTracking/N_queen.java
+++ BackTracking/N_queen.java
@@ -1,35 +1,39 @@
-public class N_queen{
-    public static void NQueen(char board[][],int row){
-        //base case
-        if(row==board.length){
-        printBoard(board);
-        return;
+public class N_queen {
+    public static void NQueen(char board[][], int row) {
+        // base case
+        if (row == board.length) {
+            printBoard(board);
+            return;
         }
-        //column loop
-    for(int j=0;j<board.length;j++){
-    board[row][j] = 'Q';
-    NQueen(board,row+1);
-    board[row][j] = 'x';
+        // column loop
+        for (int j = 0; j < board.length; j++) {
+            board[row][j] = 'Q';
+            System.out.println(board[row][j]);
+            NQueen(board, row + 1);
+            System.out.println("Empty board");
+            board[row][j] = 'x';
+        }
     }
-    }
-    public static void printBoard(char board[][]){
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board.length;j++){
-                System.out.print(board[i][j]+" ");
+
+    public static void printBoard(char board[][]) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                System.out.print(board[i][j] + " ");
             }
             System.out.println();
         }
         System.out.println();
     }
-public static void main(String[] args) {
-    int n = 2;
-    char[][] board = new char[n][n];
-    //initialize the board with 0
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            board[i][j] = 'X';
+
+    public static void main(String[] args) {
+        int n = 2;
+        char[][] board = new char[n][n];
+        // initialize the board with 0
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = 'X';
+            }
         }
+        NQueen(board, 0);
     }
-    NQueen(board, 0);
-}
 }
--- expo_practice.java
+++ expo_practice.java
@@ -1,10 +1,11 @@
 
 public class expo_practice {
     public static void findPermutation(String str, String ans) {
-        if(str.length() == 0) {
+        if (str.length() == 0) {
             System.out.println(ans);
             return;
         }
+        System.out.println("call the main function");
         for (int i = 0; i < str.length(); i++) {
             char currchar = str.charAt(i);
             String newStar = str.substring(0, i) + str.substring(i + 1);
@@ -15,6 +16,8 @@ public class expo_practice {
     public static void main(String[] args) {
         // Learning about the nqueen problem
         String str = "abc";
+        // String sub = str.substring(1);
+        // System.out.println(sub);
         findPermutation(str, "");
     }
 }
