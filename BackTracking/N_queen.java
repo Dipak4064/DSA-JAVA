1108cc11b6cf64aa36d9c3382ac9ba29b8a272cf
Dipak4064 <orraasss@gmail.com>
2025-01-26 09:43:57 +0545

    file update

--- BackTracking/N_queen.java
+++ BackTracking/N_queen.java
@@ -1,21 +1,23 @@
 public class N_queen {
-    public static void NQueen(char board[][], int row) {
-        // base case
+    public static void findPermutation(char board[][], int row) {
+        // Base case
+        System.out.println("row: " + row);
         if (row == board.length) {
-            printBoard(board);
+            printboard(board);
             return;
         }
-        // column loop
+        // Recursive case
         for (int j = 0; j < board.length; j++) {
+            System.out.println(row + " " + j);
             board[row][j] = 'Q';
-            System.out.println(board[row][j]);
-            NQueen(board, row + 1);
-            System.out.println("Empty board");
+            findPermutation(board, row + 1);
+            System.out.println(row + " " + j + "-->i am Empty");
             board[row][j] = 'x';
         }
+
     }
 
-    public static void printBoard(char board[][]) {
+    public static void printboard(char board[][]) {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
                 System.out.print(board[i][j] + " ");
@@ -26,14 +28,14 @@ public class N_queen {
     }
 
     public static void main(String[] args) {
-        int n = 2;
+        int n = 3;
         char[][] board = new char[n][n];
-        // initialize the board with 0
+        // initialize the board with 'x'
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                board[i][j] = 'X';
+                board[i][j] = 'x';
             }
         }
-        NQueen(board, 0);
+        findPermutation(board, 0);
     }
 }
--- expo_practice.java
+++ expo_practice.java
@@ -1,23 +1,69 @@
+import java.util.Scanner;
 
 public class expo_practice {
-    public static void findPermutation(String str, String ans) {
-        if (str.length() == 0) {
-            System.out.println(ans);
+    public static void NQueen(char[][] board, int row) {
+        if (row == board.length) {
+            // calling printBoard Method
+            printBoard(board);
             return;
         }
-        System.out.println("call the main function");
-        for (int i = 0; i < str.length(); i++) {
-            char currchar = str.charAt(i);
-            String newStar = str.substring(0, i) + str.substring(i + 1);
-            findPermutation(newStar, ans + currchar);
+        for (int j = 0; j < board.length; j++) {
+            // if isSafe give true in execute
+            if (isSafe(board, row, j)) {
+                board[row][j] = 'Q';
+                NQueen(board, row + 1);
+                board[row][j] = 'X';
+
+            }
         }
     }
 
+    // Finding the current place of queen will appropriate or not
+    public static boolean isSafe(char[][] board, int row, int col) {
+        // upper case
+        for (int i = row - 1; i >= 0; i--) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+        // left diagonal case
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        // right diagonal case
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    // print the accurate board
+    public static void printBoard(char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
-        // Learning about the nqueen problem
-        String str = "abc";
-        // String sub = str.substring(1);
-        // System.out.println(sub);
-        findPermutation(str, "");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Board size");
+        int n = sc.nextInt();
+        char[][] board = new char[n][n];
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                board[i][j] = 'X';
+            }
+        }
+        NQueen(board, 0);
+
     }
 }
